d65b85cd4380d1f48d6132cc6d0ff1a967793a97
obasola <dthompson98@gmail.com>
2021-12-14 13:08:27 -0600

    Added pinia state mgmt

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/router'
 import store from './store'
+import { createPinia } from 'pinia'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import "bootstrap/dist/css/bootstrap.min.css"
@@ -12,5 +13,6 @@ app
   .use(store)
   .use(router)
   .use(VueAxios, axios)
+  .use(createPinia())
   .provide('axios', app.config.globalProperties.axios)  // provide 'axios'
   .mount('#app')
