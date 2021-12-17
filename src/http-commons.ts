ab337860dd81a03b0cdd33444216ac8180bcbb52
obasola <dthompson98@gmail.com>
2021-12-16 18:01:25 -0600

    assign IP of remote server to http client

--- src/http-commons.ts
+++ src/http-commons.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const apiClient: AxiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "http://10.170.1.105:3000",
   headers: {
     "Content-type": "application/json",
   },
