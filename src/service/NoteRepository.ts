e76a813759292eeeaa8efe94717ba067a1d9462b
obasola <dthompson98@gmail.com>
2021-12-26 10:09:39 -0600

    Change remaining repository classes to use new  code pattern

--- src/service/NoteRepository.ts
+++ src/service/NoteRepository.ts
@@ -1,100 +1,25 @@
-import Note from "@/types/NoteType";
-import http from "../http-commons";
+import axios from 'axios'
+import NoteType from "../types/NoteType";
 
-const baseUR = "http://localhost:8080"
-
-class NoteRepository {
-
-  noteRecord: Note = {
-    id: 0,
-    categoryId: 0,
-    title: "",
-    desc: "",
-    dateRecorded: new Date(),
-    dateModified: new Date(),
-    personId: 0
-  };
-
-  notes = [];
-
-  findAllNotes() {
-    http.get("/notes")
-      .then((response) => {
-        this.notes = response.data;
-        console.log(response.data);
-        return this.notes;
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-      return [];       
-    }
-
-  getAll(): Note{
-    http
-      .get(baseUR + "/notes")
-      .then(response => (
-        this.mapData(response.data)
-      ))
-      .catch(error => console.log(error))
-    return this.noteRecord;
+const apiClient = axios.create({
+  baseURL: "http://10.170.1.105:3000",
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
   }
+})
 
-  get(id: number):Note {
-    http
-      .get(baseUR + `/note/${id}`)
-      .then(response => (
-        this.mapData(response.data)
-      ))
-      .catch(error => console.log(error))
-    return this.noteRecord;
-  }
-
-  create(data: Response):Note {
-    http.post("/note", data)
-    .then(response => (
-      this.mapData(response.data)
-    ))
-    .catch(error => console.log(error))
-    return this.noteRecord;
-  }
-
-  update(id: number, data: Response):Note {
-    http.put(`/note/${id}`, data)
-    .then(response => (
-      this.mapData(response.data)
-    ))
-    .catch(error => console.log(error))
-    return this.noteRecord;
-  }
-
-  delete(id: number):void {
-    http.delete(`/note/${id}`);
-  }
-
-  deleteAll():void {
-    http.delete(`/notes`);
-  }
-
-  findByCode(code: string):Note{
-    http.get(`/note?code=${code}`)
-    .then(response => (
-      this.mapData(response.data)
-    ))
-    .catch(error => console.log(error))
-    return this.noteRecord;
-  }
-
-  mapData(data: Note): Note {
-    this.noteRecord.title = data.title;
-    this.noteRecord.desc = data.desc;
-    this.noteRecord.dateModified = data.dateModified;
-    this.noteRecord.dateRecorded = data.dateRecorded;
-    this.noteRecord.id = data.id;
-    this.noteRecord.personId = data.personId;
-    return this.noteRecord;
+export default {
+  getAllNotes() {
+    return apiClient.get('/notes')
+  },
+  getNote(id: number) {
+    return apiClient.get('/notes/' + id)
+  },
+  postNote(note: NoteType) {
+    return apiClient.post('/notes', note)
   }
 }
-export default new NoteRepository();
 
 
